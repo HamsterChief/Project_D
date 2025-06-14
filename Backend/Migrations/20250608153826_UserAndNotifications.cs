f7ab8a857f02ffb0132beea457c045779c9337cc
TahaA-7 <152402600+TahaA-7@users.noreply.github.com>
2025-06-14 15:51:12 +0200

    Merging Luca branch into Main

---
+++ Backend/Migrations/20250608153826_UserAndNotifications.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAndNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_formItems_Users_UserId",
+                table: "formItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_taskItems_Users_UserId",
+                table: "taskItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_taskItems_UserId",
+                table: "taskItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_formItems_UserId",
+                table: "formItems");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVerified",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "NotificationSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EmailAllowed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PhoneAllowed = table.Column<bool>(type: "INTEGER", nullable: false),
+                    PushAllowed = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotificationSettings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PreferredColor = table.Column<string>(type: "TEXT", nullable: false),
+                    Font = table.Column<string>(type: "TEXT", nullable: false),
+                    Background = table.Column<string>(type: "TEXT", nullable: false),
+                    IconStyle = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserSettings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationSettings_UserId",
+                table: "NotificationSettings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSettings_UserId",
+                table: "UserSettings",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationSettings");
+
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_taskItems_UserId",
+                table: "taskItems",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_formItems_UserId",
+                table: "formItems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_formItems_Users_UserId",
+                table: "formItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_taskItems_Users_UserId",
+                table: "taskItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Backend/Migrations/20250608192336_VerificationAdded.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class VerificationAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VerificationCodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    VerificationCodeStr = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationCodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VerificationCodes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationCodes_UserId",
+                table: "VerificationCodes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VerificationCodes");
+        }
+    }
+}
